1854582b86dcc4d5a0505bd85b20476741af3cd4
Traveler59 <dmitry_alekseevich@mail.ru>
2016-11-01 14:40:40 +0300

    Initial commit

---
+++ src/script.ts
@@ -0,0 +1,62 @@
+var pixelSize = 10, placeSize = pixelSize + 2;
+var placesCount = {
+    x : 0,
+    y : 0,
+    allPlaces : function(): number {
+        return this.x * this.y;
+    }
+};
+
+var places = {
+    begin :  {x: 0, y: 0},
+    end:  {x: 0, y: 0}
+};
+
+
+var dragStart:InteractEvent, dragEnd:InteractEvent;
+var canvas = <HTMLCanvasElement> document.getElementById('canvas');
+canvas.width = document.body.clientWidth;
+canvas.height = window.innerHeight * 0.7;
+
+var context = canvas.getContext('2d');
+
+interact('#canvas')
+  .draggable({
+    snap: {
+        targets: [ interact.createSnapGrid({
+          x: placeSize, y: placeSize
+        }) ]
+    },
+    maxPerElement: Infinity
+  })
+  .on('dragstart', function (event: InteractEvent) {
+    dragStart = event;
+    context.fillRect(dragStart.pageX - placeSize / 2, dragStart.pageY - placeSize / 2, placeSize, placeSize);
+  })
+  .on('dragmove', function (event: InteractEvent) {
+    dragEnd = event;
+    placesCount.x = (dragStart.pageX - dragEnd.pageX)/ (placeSize);
+    placesCount.y = (dragStart.pageY - dragEnd.pageY)/ (placeSize);
+
+
+    console.log(dragStart.pageX, dragStart.pageY, dragEnd.pageX, dragEnd.pageY);
+
+
+    places.begin.x = Math.min(dragStart.pageX, dragEnd.pageX);
+    places.begin.y = Math.min(dragStart.pageY, dragEnd.pageY);
+    places.end.x = Math.max(dragStart.pageX, dragEnd.pageX);
+    places.end.y = Math.max(dragStart.pageY, dragEnd.pageY);
+
+    console.log(places.begin.x, places.begin.y, places.end.x, places.end.y);
+
+    drawSquares();
+
+  })
+  .on('doubletap', function (event: InteractEvent) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  });
+
+function drawSquares() : void{
+    context.fillRect(dragEnd.pageX - pixelSize / 2, dragEnd.pageY - pixelSize / 2,
+                 pixelSize, pixelSize);
+}
