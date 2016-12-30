7c3920868813b06144c9ef0a0fb4986fd104524d
Traveler59 <dmitry_alekseevich@mail.ru>
2016-12-30 17:39:25 +0300

    Zoom and pan works

--- src/ts/Coords.ts
+++ src/ts/Coords.ts
@@ -1,15 +1,20 @@
 class Coords {
     static empty = new Coords(0, 0);
+
     constructor(readonly x: number, readonly y: number) {}
+
     isAfterOrEqual(point: Coords): boolean {
         return ( this.x >= point.x && this.y >= point.y );
     }
+
     isBeforeOrEqual(point: Coords): boolean {
         return( this.x <= point.x && this.y <= point.y );
     }
+
     isEqual(point: Coords): boolean {
         return( this.x === point.x && this.y === point.y );
     }
+
     roundToScale(scale: number): Coords {
         return new Coords(
             Math.round(this.x / scale) * scale,
@@ -17,6 +22,7 @@ class Coords {
         );
     }
 }
+
 interface Area {
     begin: Coords;
     end: Coords;
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,18 +6,25 @@ const app = document.getElementById("app");
 class SelectorViewModel {
     auditorium: SeatSelector;
     mode: KnockoutObservable<Mode>;
+
     constructor() {
         const canv = document.getElementsByTagName("canvas")[0];
         canv.width = document.body.clientWidth * 0.98;
         canv.height = window.innerHeight * 0.7;
-
-        this.mode = ko.observable(Mode.select);
+        canv.addEventListener('wheel', this.onWheel.bind(this));
+        this.mode = ko.observable(Mode.Select);
         try {
             this.auditorium = new SeatSelector(canv, 30, 45);
         } catch(error) {
             console.log("Wrong context type");
         }
     }
+
+    onWheel(event: WheelEvent) {
+        if(event.deltaY > 0) this.auditorium.scaleDown();
+        else this.auditorium.scaleUp();
+    }
+
     logJSON(): void {
         const AppJSON = JSON.stringify(this.auditorium.toJSON());
         console.log(AppJSON);
